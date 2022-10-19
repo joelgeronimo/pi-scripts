5c05fda4f595bdcd915f9d02663a05904b84e5e6
Joel Geronimo <joel.geronimo@gmail.com>
2022-10-19 20:34:57 +0800

    Added backup script and updated README

--- README.md
+++ README.md
@@ -5,7 +5,20 @@ pi-scripts is my attempt to collate my personal work that was created to maximiz
 
 ## Scripts
 
+- [backup](#backup)
 - [system-check](#system-check)
+
+### backup
+Since I had the unfortunate incident of an SD card failing on a previous build, it dawned on me the monumental task of trying to recall and replicate configurations for my installed applications. Since these settings constantly change, I created this script to completely copy and backup my whole `Applications` folder which contains the configurations files installed on the pi. Not all settings are saved on this folder though, at least for now. So if in case disaster does strike, I would not be starting completely from scratch.
+
+The script creates a folder to an NFS mounted folder on the pi with a naming convention of `YYYYMMDD`. It utilizes `rsync` to copy the entire `Applications` folder to that newly created folder. The script then checks the allowed number of copies. If if exceeds the retention, it deletes the oldest version.
+
+This task is scheduled to run every Sunday at 21:00.
+
+This script is added on crontab (as user pi) `crontab -e`, then add the following line:
+```sh
+@reboot sleep 20; python3 /home/pi/Applications/backup.py &
+```
 
 ### system-check
 This script was born out of necessity. As the number of running docker containers increased, maintaining a healthy and free swap space became one of the solutions to avoid the pi from freezing. If that was not enough, I added a check on the average CPU load (on 5 minute intervals) as well, to verify if there are ghost applications consuming too much CPU cycles (as this has happened before: I cannot stop the deluge container).
@@ -13,6 +26,8 @@ This script was born out of necessity. As the number of running docker container
 Therefore, if any of those 2 conditions were met, the pi would reboot.
 
 You may want to avert your eyes on the usage of the `os.system("sudo shutdown -r now")` LOL.
+
+This task is scheduled to run every 5 minutes.
 
 This script is added on crontab (as user pi) `crontab -e`, then add the following line:
 ```sh
---
+++ backup.py
@@ -0,0 +1,90 @@
+import os
+import time
+import schedule
+import subprocess
+import logging
+from logging.handlers import TimedRotatingFileHandler
+from datetime import datetime
+
+SOURCE = "/home/pi/Applications/"
+DESTINATION = "/home/pi/Backups/RPi/"
+RETENTION = 4
+DATE_FORMAT = "%Y%m%d"
+
+# Logger constants
+LOGGER = "backuplog"
+LOG_LEVEL = "DEBUG"
+LOG_PATH = "/tmp/backup.log"
+LOG_WHEN = "d"
+LOG_INTERVAL = 1
+LOG_BACKUP_COUNT = 2
+
+
+def backup():
+    logger = logging.getLogger(LOGGER)
+    create_new_backup = True
+    backup_dates = []
+    current_date = datetime.today()
+
+    # List the current backup folders
+    for dirs in os.listdir(DESTINATION):
+        old_backup = os.path.join(DESTINATION, dirs)
+        if os.path.isdir(old_backup):
+            logger.info("Old backup folder: {}".format(dirs))
+            # Get the date value of the folder and append to the list
+            old_date = datetime.strptime(dirs, DATE_FORMAT)
+            backup_dates.append(old_date)
+            if old_date.strftime(DATE_FORMAT) == current_date.strftime(DATE_FORMAT):
+                create_new_backup = False
+
+    # Sort the dates
+    backup_dates_sorted = sorted(backup_dates)
+
+    # Check the retention
+    backup_dates_sorted_len = len(backup_dates_sorted)
+    if backup_dates_sorted_len >= RETENTION:
+        # The number of backups exceed the set retention. Delete as necessary.
+        # We add 1 to the sorted len to allocate the space for the new backup folder.
+        num_delete = (backup_dates_sorted_len + 1) - RETENTION
+        for x in range(num_delete):
+            # Pop the first element in the list
+            backup_to_delete = backup_dates_sorted.pop(0).strftime(DATE_FORMAT)
+            # Delete
+            logger.info("Deleting backup folder due to retention: {}".format(backup_to_delete))
+            p = subprocess.Popen(["rm", "-rf", os.path.join(DESTINATION, backup_to_delete)], stdout=subprocess.PIPE)
+            p.communicate()
+
+    if create_new_backup:
+        # Create a new backup folder
+        new_backup = os.path.join(DESTINATION, current_date.strftime(DATE_FORMAT))
+        if not os.path.exists(new_backup):
+            logger.info("Creating new backup folder: {}".format(new_backup))
+            os.makedirs(new_backup)
+
+        # Perform the RSYNC and place it in the new backup folder
+        logger.info("Performing rsync...")
+        p = subprocess.Popen(["rsync", "-rz", SOURCE, new_backup], stdout=subprocess.PIPE)
+        p.communicate()
+        logger.info("rsync done.")
+    else:
+        logger.info("Backup folder already exists, rsync skipped.")
+
+
+if __name__ == "__main__":
+    logger = logging.getLogger(LOGGER)
+    logger.setLevel(getattr(logging, LOG_LEVEL))
+    handler = TimedRotatingFileHandler(LOG_PATH,
+                                       when=LOG_WHEN,
+                                       interval=LOG_INTERVAL,
+                                       backupCount=LOG_BACKUP_COUNT)
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
+
+    # Set the backup frequency
+    schedule.every().sunday.at("21:00").do(backup)
+
+    logger.info("Backup script is now running!")
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
